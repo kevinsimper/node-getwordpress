7c21beb8f520157d8983b352e054e81bf147289e
Keith Morris <keith.morris@moxieusa.com>
2014-11-08 18:51:15 -0500

    make stylistic changes based on maintainer's feedback to pull request #1
    - remove extraneous console.log()
    - remove extra `\n` in output
    - change tabs to spaces

--- index.js
+++ index.js
@@ -5,29 +5,28 @@ var fs = require('fs.extra')
 ,   progress = require('download-status');
 
 var latestWordpress = 'http://wordpress.org/latest.zip';
-console.log(); // blank line to make output cleaner
 var subfolder = '';
 if(process.argv[2]){
   subfolder = '/' + process.argv[2];
-  console.log('Installing Wordpress in a subfolder: .' + subfolder + '\n');
+  console.log('Installing Wordpress in a subfolder: .' + subfolder);
 } else {
-  console.log('Installing Wordpress in current directory.\n')
+  console.log('Installing Wordpress in current directory.')
 }
 var finalFolder = process.cwd() + subfolder;
 fs.mkdirpSync(finalFolder);
 
 var download = new Download({
-	extract: true,
-	strip  : 1
+    extract: true,
+    strip  : 1
 })
-	.get(latestWordpress)
-	.dest(finalFolder)
-	.use(progress());
+    .get(latestWordpress)
+    .dest(finalFolder)
+    .use(progress());
 
 download.run(function (err, files, stream) {
-	if (err) {
-		console.log('fejl', err);
-	} else {
-		console.log('Wordpress downloaded and ready!\n');
-	}
+    if (err) {
+        console.log('fejl', err);
+    } else {
+        console.log('Wordpress downloaded and ready!');
+    }
 });
